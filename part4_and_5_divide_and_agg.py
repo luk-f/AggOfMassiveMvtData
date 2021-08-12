b0e115ec07981a2c8bc4b4665d92cc1fe9dbaf6c
luk-f <lucasfoulon@gmail.com>
2021-08-12 15:34:11 +0200

    add option without_interchange
    to consider or not interchange

--- part4_and_5_divide_and_agg.py
+++ part4_and_5_divide_and_agg.py
@@ -9,10 +9,12 @@ import datetime
 if __name__ == "__main__":
     
     # parameters
-    maxRadius = 0.1
-    # region = "liege"
-    region = "wallonie"
-    apply_algo_3 = True
+    maxRadius = 0.05
+    region = "liege"
+    # region = "wallonie"
+    apply_algo_3 = True # choose between "algo 2" and "algo 2 & 3"
+    without_interchange = True # choose if consider interchanges or not
+
     number_dec = str(maxRadius-int(maxRadius))[2:]
     folder_name = f"{region}_0{number_dec}"
     if apply_algo_3:
@@ -30,22 +32,29 @@ if __name__ == "__main__":
                              f'{start_date.strftime("%Y_%m_%d_%H_%M_%S")}__{end_date.strftime("%Y_%m_%d_%H_%M_%S")}.csv', 
                              index_col=0)
 
+    ## filter travel by STOPS list
     df_pt_traveleg_light = df_pt_traveleg[(df_pt_traveleg.BOARDINGSTOPID.isin(df_stops.index)) 
                                         & (df_pt_traveleg.ALIGHTINGSTOPID.isin(df_stops.index))]
 
     max_num_cluster = df_stops.CENTROID_NUMBER.max()
     stops_to_num_cluster = {}
+    ## init matrice to count link between cluster
     count_link_between_clusters = np.zeros((max_num_cluster+1, max_num_cluster+1), dtype=np.uint64)
 
     for index, row in df_stops.iterrows():
         stops_to_num_cluster[index] = row.CENTROID_NUMBER
 
+    prefix = "agg_mvt_"
+    ## if True, remove interchange
+    if without_interchange:
+        df_pt_traveleg_light = df_pt_traveleg_light[df_pt_traveleg_light.BOARDINGSEQUENCE == 1.0]
+        prefix += "without_interchange_"
     for _, row in df_pt_traveleg_light.iterrows():
         boardingclusterid = stops_to_num_cluster[row.BOARDINGSTOPID]
         alightingclusterid = stops_to_num_cluster[row.ALIGHTINGSTOPID]
 
-        count_link_between_clusters[boardingclusterid, alightingclusterid] += 1
-
-    pd.DataFrame(count_link_between_clusters).to_csv(os.path.join(path_data, "agg_mvt_"+\
+        count_link_between_clusters[boardingclusterid, alightingclusterid] += int(row.NBTRAVELERS)
+    
+    pd.DataFrame(count_link_between_clusters).to_csv(os.path.join(path_data, prefix+\
         f'{start_date.strftime("%Y_%m_%d_%H_%M_%S")}__{end_date.strftime("%Y_%m_%d_%H_%M_%S")}.csv'))
     
